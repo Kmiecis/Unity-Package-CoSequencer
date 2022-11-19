ea0bea73d61d5a62d329f89f03d846046c7b0f45
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2022-11-19 21:05:06 +0100

    Basic Graphic extensions

---
+++ Runtime/Extensions/UI/GraphicExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Common.Coroutines
+{
+    public static class GraphicExtensions
+    {
+        public static Func<IEnumerator> CoColor(this Graphic self, Color target, float duration, Func<float, float> easer = null)
+            => () => UCoroutine.YieldValueTo(self.GetColor, self.SetColor, target, duration, easer);
+
+        public static Func<IEnumerator> CoFade(this Graphic self, float target, float duration, Func<float, float> easer = null)
+            => () => UCoroutine.YieldValueTo(self.GetColorA, self.SetColorA, target, duration, easer);
+    }
+
+    internal static class InternalGraphicExtensions
+    {
+        #region Color
+        public static Color GetColor(this Graphic self)
+            => self.color;
+
+        public static void SetColor(this Graphic self, Color value)
+            => self.color = value;
+
+        public static float GetColorR(this Graphic self)
+            => self.color.r;
+
+        public static void SetColorR(this Graphic self, float value)
+            => self.color = self.color.WithR(value);
+
+        public static float GetColorG(this Graphic self)
+            => self.color.g;
+
+        public static void SetColorG(this Graphic self, float value)
+            => self.color = self.color.WithG(value);
+
+        public static float GetColorB(this Graphic self)
+            => self.color.b;
+
+        public static void SetColorB(this Graphic self, float value)
+            => self.color = self.color.WithB(value);
+
+        public static float GetColorA(this Graphic self)
+            => self.color.a;
+
+        public static void SetColorA(this Graphic self, float value)
+            => self.color = self.color.WithA(value);
+        #endregion
+    }
+}
