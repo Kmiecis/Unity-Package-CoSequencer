b9251678a7acadf23d42d0fd19eeb81c1e82ded2
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2022-11-19 20:58:15 +0100

    Tidy up

--- Runtime/Extensions/Core/IEnumeratorExtensions.cs
+++ Runtime/Extensions/Core/IEnumeratorExtensions.cs
@@ -8,71 +8,71 @@ namespace Common.Coroutines
     public static partial class IEnumeratorExtensions
     {
         #region Then
-        public static IEnumerator Then(this IEnumerator self, IEnumerator coroutine)
-        {
-            return coroutine == null ? self : UCoroutine.YieldSequentially(self, coroutine);
-        }
-
-        public static IEnumerator<T> Then<T>(this IEnumerator<T> self, IEnumerator<T> coroutine)
-        {
-            return coroutine == null ? self : UCoroutine.YieldSequentially(self, coroutine);
-        }
-
         public static IEnumerator Then(this IEnumerator self, YieldInstruction yield)
         {
-            return yield == null ? self : self.Then(UCoroutine.Yield(yield));
+            return self.Then(UCoroutine.Yield(yield));
         }
 
         public static IEnumerator Then(this IEnumerator self, Coroutine coroutine)
         {
-            return coroutine == null ? self : self.Then(UCoroutine.Yield(coroutine));
+            return self.Then(UCoroutine.Yield(coroutine));
         }
 
         public static IEnumerator Then(this IEnumerator self, Action callback)
         {
-            return callback == null ? self : self.Then(UCoroutine.Yield(callback));
+            return self.Then(UCoroutine.Yield(callback));
         }
 
         public static IEnumerator<T> Then<T>(this IEnumerator<T> self, Func<T> provider)
         {
-            return provider == null ? self : self.Then(UCoroutine.Yield(provider));
+            return self.Then(UCoroutine.Yield(provider));
         }
 
         public static IEnumerator Then(this IEnumerator self, Func<IEnumerator> provider)
         {
-            return provider == null ? self : self.Then(UCoroutine.Yield(provider));
+            return self.Then(UCoroutine.Yield(provider));
         }
 
         public static IEnumerator<T> Then<T>(this IEnumerator<T> self, Func<IEnumerator<T>> provider)
         {
-            return provider == null ? self : self.Then(UCoroutine.Yield(provider));
+            return self.Then(UCoroutine.Yield(provider));
+        }
+
+        public static IEnumerator Then(this IEnumerator self, IEnumerator coroutine)
+        {
+            return UCoroutine.YieldSequentially(self, coroutine);
+        }
+
+        public static IEnumerator<T> Then<T>(this IEnumerator<T> self, IEnumerator<T> coroutine)
+        {
+            return UCoroutine.YieldSequentially(self, coroutine);
         }
         #endregion
 
         #region With
-        public static IEnumerator With(this IEnumerator self, IEnumerator coroutine)
-        {
-            return coroutine == null ? self : UCoroutine.YieldParallel(self, coroutine);
-        }
-
         public static IEnumerator With(this IEnumerator self, YieldInstruction yield)
         {
-            return yield == null ? self : self.With(UCoroutine.Yield(yield));
+            return self.With(UCoroutine.Yield(yield));
         }
 
         public static IEnumerator With(this IEnumerator self, Coroutine coroutine)
         {
-            return coroutine == null ? self : self.With(UCoroutine.Yield(coroutine));
+            return self.With(UCoroutine.Yield(coroutine));
         }
 
         public static IEnumerator With(this IEnumerator self, Action callback)
         {
-            return callback == null ? self : self.With(UCoroutine.Yield(callback));
+            return self.With(UCoroutine.Yield(callback));
         }
 
         public static IEnumerator With(this IEnumerator self, Func<IEnumerator> provider)
         {
-            return provider == null ? self : self.With(UCoroutine.Yield(provider));
+            return self.With(UCoroutine.Yield(provider));
+        }
+
+        public static IEnumerator With(this IEnumerator self, IEnumerator coroutine)
+        {
+            return UCoroutine.YieldParallel(self, coroutine);
         }
         #endregion
 
