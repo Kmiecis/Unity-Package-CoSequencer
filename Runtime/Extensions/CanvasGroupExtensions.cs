fba0798f80f7855660a07ee26fe683c7ff8d3220
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2022-11-19 08:17:03 +0100

    Basic CanvasGroup extensions

---
+++ Runtime/Extensions/CanvasGroupExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Common.Coroutines
+{
+    public static class CanvasGroupExtensions
+    {
+        public static IEnumerator CoFade(this CanvasGroup self, float target, float duration, Func<float, float> easer = null)
+            => UCoroutine.YieldValueTo(self.GetAlpha, self.SetAlpha, target, duration, easer);
+    }
+
+    internal static class InternalCanvasGroupExtensions
+    {
+        #region Alpha
+        public static float GetAlpha(this CanvasGroup self)
+            => self.alpha;
+
+        public static void SetAlpha(this CanvasGroup self, float value)
+            => self.alpha = value;
+        #endregion
+    }
+}
