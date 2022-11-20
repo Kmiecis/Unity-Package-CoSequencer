febb357d8f88a3b32f181222e6a7faf63301c63d
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2022-11-20 06:24:35 +0100

    Basic ScrollRect extensions

---
+++ Runtime/Extensions/UI/ScrollRectExtensions.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Common.Coroutines
+{
+    public static class ScrollRectExtensions
+    {
+        public static Func<IEnumerator> CoHorizontalPosition(this ScrollRect self, float target, float duration, Func<float, float> easer = null)
+            => () => UCoroutine.YieldValueTo(self.GetHorizontalNormalizedPosition, self.SetHorizontalNormalizedPosition, target, duration, easer);
+
+        public static Func<IEnumerator> CoVerticalPosition(this ScrollRect self, float target, float duration, Func<float, float> easer = null)
+            => () => UCoroutine.YieldValueTo(self.GetVerticalNormalizedPosition, self.SetVerticalNormalizedPosition, target, duration, easer);
+
+        public static Func<IEnumerator> CoPosition(this ScrollRect self, Vector2 target, float duration, Func<float, float> easer = null)
+            => () => UCoroutine.YieldValueTo(self.GetNormalizedPosition, self.SetNormalizedPosition, target, duration, easer);
+
+        public static Func<IEnumerator> CoHorizontalVelocity(this ScrollRect self, float target, float duration, Func<float, float> easer = null)
+            => () => UCoroutine.YieldValueTo(self.GetHorizontalVelocity, self.SetHorizontalVelocity, target, duration, easer);
+
+        public static Func<IEnumerator> CoVerticalVelocity(this ScrollRect self, float target, float duration, Func<float, float> easer = null)
+            => () => UCoroutine.YieldValueTo(self.GetVerticalVelocity, self.SetVerticalVelocity, target, duration, easer);
+
+        public static Func<IEnumerator> CoVelocity(this ScrollRect self, Vector2 target, float duration, Func<float, float> easer = null)
+            => () => UCoroutine.YieldValueTo(self.GetVelocity, self.SetVelocity, target, duration, easer);
+    }
+
+    internal static class InternalScrollRectExtensions
+    {
+        #region Normalized position
+        public static float GetHorizontalNormalizedPosition(this ScrollRect self)
+            => self.horizontalNormalizedPosition;
+
+        public static void SetHorizontalNormalizedPosition(this ScrollRect self, float value)
+            => self.horizontalNormalizedPosition = value;
+
+        public static float GetVerticalNormalizedPosition(this ScrollRect self)
+            => self.verticalNormalizedPosition;
+
+        public static void SetVerticalNormalizedPosition(this ScrollRect self, float value)
+            => self.verticalNormalizedPosition = value;
+
+        public static Vector2 GetNormalizedPosition(this ScrollRect self)
+            => self.normalizedPosition;
+
+        public static void SetNormalizedPosition(this ScrollRect self, Vector2 value)
+            => self.normalizedPosition = value;
+        #endregion
+
+        #region Velocity
+        public static Vector2 GetVelocity(this ScrollRect self)
+            => self.velocity;
+
+        public static void SetVelocity(this ScrollRect self, Vector2 value)
+            => self.velocity = value;
+
+        public static float GetHorizontalVelocity(this ScrollRect self)
+            => self.velocity.x;
+
+        public static void SetHorizontalVelocity(this ScrollRect self, float value)
+            => self.velocity = self.velocity.WithX(value);
+
+        public static float GetVerticalVelocity(this ScrollRect self)
+            => self.velocity.y;
+
+        public static void SetVerticalVelocity(this ScrollRect self, float value)
+            => self.velocity = self.velocity.WithY(value);
+        #endregion
+    }
+}
