e5ddb5ed07216461fa7fe84010b92e5772003c75
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2024-02-07 05:28:25 +0100

    Update SegmentGroup.cs

--- Runtime/Sequences/Base/SegmentGroup.cs
+++ Runtime/Sequences/Base/SegmentGroup.cs
@@ -2,9 +2,9 @@ namespace Common.Coroutines
 {
     public static class SegmentGroup
     {
-        public const int Core = 10;
-        public const int UI = 100;
-        public const int Utility = 1000;
+        public const int Core = 1000;
+        public const int UI = 2000;
+        public const int Utility = 3000;
         public const int Custom = 10000;
     }
 }
