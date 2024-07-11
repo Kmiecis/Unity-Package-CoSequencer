422ae4fe567f3b049439c0da7928033d4c828103
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2024-07-11 20:14:53 +0200

    Fixed CoSequence not visible in Editor as a field

--- Editor/Sequences/CoSequencerEditor.cs
+++ Editor/Sequences/CoSequencerEditor.cs
@@ -1,6 +1,7 @@
 using Common.Coroutines;
 using System.Collections;
 using UnityEditor;
+using UnityEngine;
 
 namespace CommonEditor.Coroutines
 {
@@ -27,6 +28,20 @@ namespace CommonEditor.Coroutines
         {
             var list = GetList();
             _menu = new SegmentsMenu(list);
+        }
+    }
+
+    [CustomPropertyDrawer(typeof(CoSequencer))]
+    public class CoSequencerDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.PropertyField(position, property, label, false);
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, false);
         }
     }
 }
