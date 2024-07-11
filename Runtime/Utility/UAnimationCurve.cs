7718f0f1d4821d39fa50b0a687d92eb3c3f16f18
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2024-07-11 20:16:23 +0200

    Public AnimationCurve utility

--- Runtime/Utility/UAnimationCurve.cs
+++ Runtime/Utility/UAnimationCurve.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace Common.Coroutines
 {
-    internal static class UAnimationCurve
+    public static class UAnimationCurve
     {
         public static AnimationCurve EaseInOutNormalized()
             => AnimationCurve.EaseInOut(0.0f, 0.0f, 1.0f, 1.0f);
